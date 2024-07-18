0fe2f794403ac745d251bf90f84e5212d215d925
ericcarmi <ecarmi@protonmail.com>
2024-07-17 20:16:44 -0700

    bundle name

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { listen } from '@tauri-apps/api/event'
 
-const unlisten = listen("color", (event) => {
+listen("color", (event) => {
   let rgb = event.payload as number[]
   let r = rgb[0]
   let g = rgb[1]
@@ -8,13 +8,12 @@ const unlisten = listen("color", (event) => {
   let e = document.getElementById("root")
   if (e) {
     e.style.background = `rgb(${r}, ${g}, ${b})`
-    e.style.color = `rgb(${255-r}, ${255-g}, ${255-b})`
-    e.innerHTML=`
+    e.style.color = `rgb(${255 - r}, ${255 - g}, ${255 - b})`
+    e.innerHTML = `
     <span>${r}, ${g}, ${b}<span>
     <br/>
     <span>${r.toString(16)}${g.toString(16)}${b.toString(16)}<span>
     `
   }
-  // console.log(rgb)
-
 })
+
